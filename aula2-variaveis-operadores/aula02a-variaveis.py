2ff5d4ce792be276f2f052ab4c452f3f833412c8
kayk-7 <araujokayky874@gmail.com>
2026-03-19 22:28:57 -0300

    aula 2 variaveis e operadores

---
+++ aula2-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,29 @@
+print("olá mundo")
+
+print(7+4)
+print("7 + 4")
+print("7" + "4") #CONCATENAÇÃO DE STINGS
+
+#Comentário de 1 linha
+'''
+
+    Comentários de 
+    múltiplas linhas
+'''
+
+#VARIÁVEIS
+nome = "Alexadre" # srt
+idade = 26 #int
+peso = 70.2 #float
+
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!!") #f (formatação)
+
+#INPUTS - SIMULAÇÃO DE FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
